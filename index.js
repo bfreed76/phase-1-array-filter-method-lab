8a88f7f7b4997c7026308b8fab45b2a4b6e81f10
Brinton <brintonfoyreed@gmail.com>
2021-04-09 08:58:30 -0700

    commit tree c171261803bdb6cfcfd6aa9cdc86be4cca37d874

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+let findMatching = (arr, name) => {
+    const value = arr.filter((full) => {return full.toLowerCase() === name.toLowerCase()})
+    console.log(value); 
+    return value;
+};
+
+let fuzzyMatch = (arr, name) => {
+    const value = arr.filter((firstTwo) => {return firstTwo.slice(0,2) === name}) 
+    console.log(value);
+    return value;
+}
+
+let matchName = (arr, name) => {
+    const value = arr.filter((element) => {return element.name === name})
+    console.log(value[0]["hometown"]);
+    return value;
+}
